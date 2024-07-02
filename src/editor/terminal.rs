88ca5d3b8c18d9af9c006232bf5a26a87ede71ae
GauravN0910 <root@Gaurav>
2024-07-02 19:04:45 +0530

    Display Message and Safe Arithmetics

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,34 +1,80 @@
-use crossterm::cursor::MoveTo;
-use crossterm::execute;
+use crossterm::cursor::{Hide, MoveTo, Show};
+use crossterm::queue;
+use crossterm::style::Print;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType};
-use std::io::stdout;
+use std::io::{stdout, Error, Write};
+use core::fmt::Display;
 
-pub struct Terminal{}
+#[derive(Copy, Clone)]
+pub struct Size {
+    pub height: usize,
+    pub width: usize,
+}
 
-impl Terminal{
-    pub fn terminate () -> Result<(), std::io::Error>{
+#[derive(Copy, Clone)]
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+pub struct Terminal;
+
+impl Terminal {
+    pub fn terminate() -> Result<(), Error> {
+        Self::execute()?;
         disable_raw_mode()?;
         Ok(())
     }
 
-    pub fn initialize () -> Result<(), std::io::Error>{
+    pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(0, 0)?;
+        Self::move_cursor_to(Position { x: 0, y: 0 })?;
+        Self::execute()?;
         Ok(())
     }
 
-    pub fn clear_screen() -> Result<(), std::io::Error>{
-        execute!(stdout(), Clear(ClearType::All))?;
+    pub fn clear_screen() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::All))?;
         Ok(())
     }
 
-    pub fn move_cursor_to(x: u16, y: u16) -> Result<(), std::io::Error>{
-        execute!(stdout(), MoveTo(x, y))?;
+    pub fn clear_line() -> Result<(), Error> {
+        queue!(stdout(), Clear(ClearType::CurrentLine))?;
         Ok(())
     }
 
-    pub fn size() -> Result<(u16, u16), std::io::Error>{
-        size()
+    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+        #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
+        queue!(stdout(), MoveTo(position.x as u16, position.y as u16))?;
+        Ok(())
     }
+
+    pub fn hide_cursor() -> Result<(), Error> {
+        queue!(stdout(), Hide)?;
+        Ok(())
+    }
+
+    pub fn show_cursor() -> Result<(), Error> {
+        queue!(stdout(), Show)?;
+        Ok(())
+    }
+
+    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+        queue!(stdout(), Print(string))?;
+        Ok(())
+    }
+
+    pub fn size() -> Result<Size, Error> {
+        let (width_u16, height_u16) = size()?;
+        #[allow(clippy::as_conversions)]
+        let height = height_u16 as usize;
+        let width = width_u16 as usize;
+        Ok(Size { height, width})
+    }
+
+    pub fn execute() -> Result<(), Error> {
+        stdout().flush()?;
+        Ok(())
+    }
+
 }
