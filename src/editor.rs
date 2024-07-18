1e153155625cb1f940561ace548c165d3245c957
GauravN0910 <root@Gaurav>
2024-07-18 19:46:45 +0530

    Scrolling

--- src/editor.rs
+++ src/editor.rs
@@ -1,60 +1,49 @@
-use core::{cmp::min, panic};
-use crossterm::event::{
-    read, Event, KeyCode, 
-    KeyEvent, KeyEventKind, KeyModifiers,
-}; 
+use crossterm::event::{read, Event, KeyEvent, KeyEventKind};
 use std::{
-    env, 
+    env,
     io::Error,
-    panic::{set_hook, take_hook}};
+    panic::{set_hook, take_hook},
+};
+mod editorcommand;
 mod terminal;
 mod view;
-use terminal::{Position, Size, Terminal};
+use terminal::Terminal;
 use view::View;
 
-#[derive(Copy, Clone, Default)]
-struct Location {
-    x: usize,
-    y: usize,
-}
-
+use editorcommand::EditorCommand;
 
 pub struct Editor {
     should_quit: bool,
-    location: Location,
     view: View,
 }
 
 impl Editor {
-    pub fn new() -> Result<Self, Error>{
+    pub fn new() -> Result<Self, Error> {
         let current_hook = take_hook();
-        set_hook(Box::new(move|panic_info| {
-            let _= Terminal::terminate();
+        set_hook(Box::new(move |panic_info| {
+            let _ = Terminal::terminate();
             current_hook(panic_info);
         }));
-
         Terminal::initialize()?;
         let mut view = View::default();
         let args: Vec<String> = env::args().collect();
         if let Some(file_name) = args.get(1) {
             view.load(file_name);
         }
-        Ok(Self{
+        Ok(Self {
             should_quit: false,
-            location: Location::default(),
-            view
+            view,
         })
     }
-
     pub fn run(&mut self) {
         loop {
             self.refresh_screen();
-            if self.should_quit{
+            if self.should_quit {
                 break;
             }
-            match read(){
+            match read() {
                 Ok(event) => self.evaluate_event(event),
-                Err(err) =>{
+                Err(err) => {
                     #[cfg(debug_assertions)]
                     {
                         panic!("Could not read event: {err:?}");
@@ -63,89 +52,47 @@ impl Editor {
             }
         }
     }
-    fn move_point(&mut self, key_code: KeyCode){
-        let Location { mut x, mut y } = self.location;
-        let Size { height, width } = Terminal::size().unwrap_or_default();
-        match key_code {
-            KeyCode::Up => {
-                y = y.saturating_sub(1);
-            }
-            KeyCode::Down => {
-                y = min(height.saturating_sub(1), y.saturating_add(1));
-            }
-            KeyCode::Left => {
-                x = x.saturating_sub(1);
-            }
-            KeyCode::Right => {
-                x = min(width.saturating_sub(1), x.saturating_add(1));
-            }
-            KeyCode::PageUp => {
-                y = 0;
-            }
-            KeyCode::PageDown => {
-                y = height.saturating_sub(1);
-            }
-            KeyCode::Home => {
-                x = 0;
-            }
-            KeyCode::End => {
-                x = width.saturating_sub(1);
-            }
-            _ => (),
-        }
-        self.location = Location { x, y };
-    }
 
-    #[allow(clippy::needless_pass_by_value)]
-    fn evaluate_event(&mut self, event: Event){
-        match event {
-            Event::Key(KeyEvent {
-                code,
-                kind: KeyEventKind::Press,
-                modifiers,
-                ..
-            }) => match (code, modifiers) {
-                (KeyCode::Char('q'), KeyModifiers::CONTROL) => {
-                    self.should_quit = true;
+    fn evaluate_event(&mut self, event: Event) {
+        let should_process = match &event {
+            Event::Key(KeyEvent { kind, .. }) => kind == &KeyEventKind::Press,
+            Event::Resize(_, _) => true,
+            _ => false,
+        };
+
+        if should_process {
+            match EditorCommand::try_from(event) {
+                Ok(command) => {
+                    if matches!(command, EditorCommand::Quit) {
+                        self.should_quit = true;
+                    } else {
+                        self.view.handle_command(command);
+                    }
                 }
-                (
-                    KeyCode::Up
-                    | KeyCode::Down
-                    | KeyCode::Left
-                    | KeyCode::Right
-                    | KeyCode::PageDown
-                    | KeyCode::PageUp
-                    | KeyCode::End
-                    | KeyCode::Home,
-                    _,
-                ) => {
-                    self.move_point(code);
+                Err(err) => {
+                    #[cfg(debug_assertions)]
+                    {
+                        panic!("Could not handle command: {err}");
+                    }
                 }
-                _ => {}
-            },
-            Event::Resize(width_u16, height_u16) => {
-                #[allow(clippy::as_conversions)]
-                let height = height_u16 as usize;
-                #[allow(clippy::as_conversions)]
-                let width = width_u16 as usize;
-                self.view.resize(Size {
-                    height,
-                    width,
-                });
             }
-            _ => {}
+        } else {
+            #[cfg(debug_assertions)]
+            {
+                panic!("Received and discarded unsupported or non-press event.");
+            }
         }
     }
-    fn refresh_screen(& mut self) {
+    fn refresh_screen(&mut self) {
         let _ = Terminal::hide_caret();
         self.view.render();
-        let _ = Terminal::move_caret_to(Position{col: self.location.x, row: self.location.y});
+        let _ = Terminal::move_caret_to(self.view.get_position());
         let _ = Terminal::show_caret();
         let _ = Terminal::execute();
     }
 }
 
-impl Drop for Editor{
+impl Drop for Editor {
     fn drop(&mut self) {
         let _ = Terminal::terminate();
         if self.should_quit {
